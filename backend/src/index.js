cd22aa13ae352fa74855a808f1263bd977a63f86
Rushikesh <rushi.deshmukh2071999@gmail.com>
2025-06-16 16:21:48 +0530

    feat: build fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,6 +42,6 @@ app.use("/api/message", messageRoutes);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..frontend", "dist", "index.html"));
   });
 }
